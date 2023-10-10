dccbd866832db4b1292d950a5574d0789ac75a86
WoolenStone <wph2012.wh@gmail.com>
2023-10-10 18:30:14 -0500

    Fixed unclickable quotes
    ' was causing issues when it was used in a quote. Re-fetched the data instead of passing it to avoid this issue

--- Core.js
+++ Core.js
@@ -1,4 +1,3 @@
-
 function displayCharacters(url) {  //displays a list of charactes
     fetch(url)
         .then(res => res.json())
@@ -20,26 +19,33 @@ function displayEpisodes(url) {  //diplays a list of episode names
 
         });
 }
-function displayQuotes(url) { //displays a list of quotes
-	console.log("QuoteClicked");
+
+function displayQuotes() { //displays a list of quotes
+	url = urlBase + 'quote';
+    console.log(url);
+    console.log("QuoteClicked");
 	fetch(url)						//get the raw answer
         .then(res => res.json())			//return: object with
         .then(arr => {
+            console.log(arr);
             let innerText = "\n<tr><th>Quotes</th></tr>\n";  //This is where you start your table block
             // "<h3>Quotes</h3>\n
             // <table>\n
             // <tr><th>Quotes/th></tr>\n"
             for (let i = 0; i < arr.length; i++) {
-                let attach = "onclick='quoteClick("+'"' + arr[i].quote +'"'+','+'"' + arr[i].by + '"' + ")';";
+                let attach = "onclick='quoteClick("+'"' + url +'"'+','+'"' + i + '"' + ")';";
                 //onclick="quoteClick(arr[i].quote,arr[i].by)";
-		        innerText+="<tr><td ><a " + attach + ">" + arr[i].quote +"</a></td></tr>\n";
+		        innerText+="<tr><td ><a " + attach + ">" + arr[i].quote +"</a></tr>\n";
             }
             innerText += "\n";
             document.getElementById("display").innerHTML = innerText;
         });
 }
 
-function quoteClick(quote, quoteBy){
-    console.log(quote, quoteBy);
-    document.getElementById("display").innerHTML="<tr><td>"+quote+"<br> Said by: "+quoteBy+"<td><tr>";
+function quoteClick(url, num){
+    fetch(url)
+        .then(res => res.json())
+        .then(arr => {
+            document.getElementById("display").innerHTML="<tr><td>"+ arr[num].quote +"<br> Said by: "+ arr[num].quoteBy +"<td><tr>";
+        });
 }
